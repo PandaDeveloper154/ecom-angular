3ec6d80e4710a8306d3c7da74181abc3d5cbb80a
Tuyet <ttuyet1504@gmail.com>
2024-04-29 10:44:49 +0000

    bug details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -47,5 +47,9 @@ export class HeaderComponent implements OnInit {
   hideSearch(){
     this.searchResult= undefined
   }
+  submitSearch(val: string){
+    console.warn(val)
+    this.route.navigate([`search/${val}`])
+  }
 
 }
